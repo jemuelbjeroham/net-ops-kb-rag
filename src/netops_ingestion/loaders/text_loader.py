a5e664f0b29be358fb9e5c0e2c7fadf900ac8107
Jemuel B Jeroham <jemuelbjeroham@outlook.com>
2026-08-15 01:16:40 +0530

    feat: Implement text document loader

---
+++ src/netops_ingestion/loaders/text_loader.py
@@ -0,0 +1,14 @@
+from netops_ingestion.domain.document import Document
+from netops_ingestion.loaders.base import BaseLoader
+
+
+class TextLoader(BaseLoader):
+    """
+    This is a Loader for document of Text types
+    """
+
+    def load(self) -> list[Document]:
+        content = self.source.read_text(encoding="utf-8")
+        document = Document(content=content)
+        return [document]
+
